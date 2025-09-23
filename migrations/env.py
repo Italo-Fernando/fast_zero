0d4d6be54507ae458220255785f8423e4767221b
Italo-Fernando <main.italofernando@gmail.com>
2025-09-23 09:51:43 -0300

    alteraçoes no commit anterior

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import asyncio
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -8,8 +9,6 @@ from alembic import context
 
 from fast_zero.models import table_registry
 from fast_zero.settings import Settings
-
-import asyncio
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -31,7 +30,6 @@ target_metadata = table_registry.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 
 def do_run_migrations(connection): 
@@ -57,13 +55,6 @@ async def run_async_migrations():
 
     await connectable.dispose()
 
-
-def run_migrations_online(): 
-
-
-    asyncio.run(run_async_migrations())
-
-
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -88,29 +79,10 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-# def run_migrations_online() -> None:
-#     """Run migrations in 'online' mode.
+def run_migrations_online():
+    asyncio.run(run_async_migrations())
 
-#     In this scenario we need to create an Engine
-#     and associate a connection with the context.
-
-#     """
-#     connectable = engine_from_config(
-#         config.get_section(config.config_ini_section, {}),
-#         prefix="sqlalchemy.",
-#         poolclass=pool.NullPool,
-#     )
-
-#     with connectable.connect() as connection:
-#         context.configure(
-#             connection=connection, target_metadata=target_metadata
-#         )
-
-#         with context.begin_transaction():
-#             context.run_migrations()
-
-
-# if context.is_offline_mode():
-#     run_migrations_offline()
-# else:
-#     run_migrations_online()
+    if context.is_offline_mode():
+        run_migrations_offline()
+    else:
+        run_migrations_online()
